d2babf18f3e505dfe5b25fc25fe7868442ccf968
sunah <1004_sunah@naver.com>
2023-11-07 12:24:17 +0900

    createdAt

--- routes/products.router.js
+++ routes/products.router.js
@@ -12,10 +12,7 @@ router.post(api_basic, async (req, res) => {
   const { title, content, author, password } = req.body;
   const products = await Products.find({}).sort({ id: -1 });
   const id = products.length === 0 ? 1 : Number(products[0].id) + 1;
-  const seoulTimeOffset = 9 * 60; // 한국 시간대 (UTC+9)의 분 단위 오프셋
-  const currentDate = new Date();
-  const seoulTime = new Date(currentDate.getTime() + seoulTimeOffset * 60000); // 시간 오프셋 적용
-  
+
   // UTC 형식으로 저장
   const utcDateString = seoulTime.toISOString();
   console.log(utcDateString);
@@ -27,7 +24,7 @@ router.post(api_basic, async (req, res) => {
       content,
       author,
       status: "FOR_SALE",
-      createdAt:utcDateString,
+      createdAt: new Date(),
     });
     res.json({ message: "판매 상품을 등록하였습니다." });
   } catch (err) {
@@ -54,22 +51,22 @@ router.get(api_basic, async (req, res) => {
 //상품 상세조회
 router.get(api_detail, async (req, res) => {
   const { productId } = req.params;
-  if(typeof productId !== Number){
+  if (typeof productId !== Number) {
     res.status(400).json({ message: "데이터 형식이 올바르지 않습니다." });
-  }else{
+  } else {
     const products = await Products.findOne({ id: productId });
     if (products == null) {
       res.status(404).json({ message: "상품조회에 실패하였습니다." });
     } else {
       try {
-        const result ={
-            id: products._id,
-            title: products.title,
-            content: products.content,
-            author: products.author,
-            status: products.status,
-            createdAt: products.createdAt,
-          };
+        const result = {
+          id: products._id,
+          title: products.title,
+          content: products.content,
+          author: products.author,
+          status: products.status,
+          createdAt: products.createdAt,
+        };
         res.json({ date: result });
       } catch (err) {
         console.error(err);
@@ -94,11 +91,13 @@ router.put(api_detail, async (req, res) => {
       .json({ message: "상품을 수정할 권한이 존재하지 않습니다." });
   } else {
     try {
-      await Products.updateOne({id : productId,$set: {
-        title,
-        content,
-        status,
-      }});
+      await Products.updateOne({
+        id: productId, $set: {
+          title,
+          content,
+          status,
+        }
+      });
       res.send({ message: "상품 정보를 수정하였습니다." });
     } catch (err) {
       console.log(err);
@@ -111,9 +110,9 @@ router.put(api_detail, async (req, res) => {
 router.delete(api_detail, async (req, res) => {
   const { productId } = req.params;
   const { password } = req.body;
-  
+
   const products = await Products.find({ id: Number(productId) });
-  
+
   if (!products.length) {
     res.status(404).json({ message: "상품 조회에 실패하였습니다." });
   } else if (password !== products[0].password) {
